567a0da6118a23336996246bc7328ec68c860c08
Brij Pratap Singh <brijjadaun@gmail.com>
2020-02-18 01:11:53 +0530

    Initial Commit

---
+++ app/src/main/java/com/brijpratapsingh/volleyexample/ImageClass.java
@@ -0,0 +1,42 @@
+package com.brijpratapsingh.volleyexample;
+
+import android.graphics.Bitmap;
+import android.util.LruCache;
+//import android.support.v4.util.LruCache;
+
+//import androidx.collection.LruCache;
+
+import com.android.volley.toolbox.ImageLoader.ImageCache;
+
+public class ImageClass extends LruCache<String, Bitmap> implements
+        ImageCache {
+    public static int getDefaultLruCacheSize() {
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        final int cacheSize = maxMemory / 8;
+
+        return cacheSize;
+    }
+
+    public ImageClass() {
+        this(getDefaultLruCacheSize());
+    }
+
+    public ImageClass(int sizeInKiloBytes) {
+        super(sizeInKiloBytes);
+    }
+
+    @Override
+    protected int sizeOf(String key, Bitmap value) {
+        return value.getRowBytes() * value.getHeight() / 1024;
+    }
+
+    @Override
+    public Bitmap getBitmap(String url) {
+        return get(url);
+    }
+
+    @Override
+    public void putBitmap(String url, Bitmap bitmap) {
+        put(url, bitmap);
+    }
+}
--- app/src/main/java/com/brijpratapsingh/volleyexample/MainActivity.java
+++ app/src/main/java/com/brijpratapsingh/volleyexample/MainActivity.java
@@ -2,49 +2,69 @@ package com.brijpratapsingh.volleyexample;
 
         import androidx.appcompat.app.AppCompatActivity;
 
+        import android.app.ProgressDialog;
         import android.os.Bundle;
         import android.util.Log;
         import android.view.View;;
         import android.widget.Button;
+        import android.widget.ListView;
         import android.widget.Toast;
 
         import com.android.volley.Request;
         import com.android.volley.RequestQueue;
         import com.android.volley.Response;
         import com.android.volley.VolleyError;
+        import com.android.volley.VolleyLog;
+        import com.android.volley.toolbox.JsonArrayRequest;
         import com.android.volley.toolbox.StringRequest;
         import com.android.volley.toolbox.Volley;
 
+        import org.json.JSONArray;
+        import org.json.JSONException;
+        import org.json.JSONObject;
+
         import java.io.StringReader;
+        import java.io.UnsupportedEncodingException;
+        import java.util.ArrayList;
+        import java.util.List;
 
 public class MainActivity extends AppCompatActivity
 {
     private static final String TAG=MainActivity.class.getName();
-    private Button requestBtn;
+    //private Button requestBtn;
     private RequestQueue requestQueue;
     private StringRequest stringRequest;
+    private ProgressDialog pDialog;
+    private List<Model> modelList = new ArrayList();
+    private ListView listView;
+    private CustomAdapter adapter;
     //any json data url
-    private String url="https://api.myjson.com/bins/p64ng";
+    private String url="http://mobileappdatabase.in/demo/smartnews/app_dashboard/jsonUrl/single-article.php?article-id=71";
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        requestBtn=findViewById(R.id.requestbtn);
-        requestBtn.setOnClickListener(new View.OnClickListener() {
+        //requestBtn=findViewById(R.id.requestbtn);
+        listView = (ListView) findViewById(R.id.list);
+        adapter = new CustomAdapter(this, modelList);
+        listView.setAdapter(adapter);
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
+        pDialog.show();
+        sendandrequestResponse();
+        /*requestBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
-                sendandrequestResponse();
-
             }
-        });
+        });*/
 
     }
 
     private void sendandrequestResponse()
     {
-        requestQueue= Volley.newRequestQueue(this);
+        /*requestQueue= Volley.newRequestQueue(this);
         stringRequest=new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
@@ -58,7 +78,58 @@ public class MainActivity extends AppCompatActivity
                 Log.i(TAG,"Error :" + error.toString());
             }
         });
-        requestQueue.add(stringRequest);
+        requestQueue.add(stringRequest);*/
+
+        JsonArrayRequest movieReq = new JsonArrayRequest(url, new Response.Listener<JSONArray>()
+        {
+            @Override
+                    public void onResponse(JSONArray response)
+            {
+                        Log.d(TAG, response.toString());
+                        hideDialog();
+
+                            // Parsing json
+                            for (int i = 0; i < response.length(); i++) {
+                                try {
+
+                                    JSONObject obj = response.getJSONObject(i);
+                                    Model model = new Model();
+                                    model.setTitle(obj.getString("title"));
+                                    model.setCategory(obj.getString("category"));
+                                    model.setThumbnailUrl(obj.getString("image"));
+
+                                    modelList.add(model);
+
+                                } catch (JSONException e)
+                                {
+                                    e.printStackTrace();
+                                }
+
+                            }
+
+
+                        // notifying list adapter about data changes so that it renders the list view with updated data
+                        adapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                hideDialog();
+
+            }
+        });
+        //Adding request to queue
+        AppController.getInstance().addToRequestQueue(movieReq);
+    }
+
+    private void hideDialog()
+    {
+        if (pDialog != null)
+        {
+            pDialog.dismiss();
+            pDialog = null;
+        }
     }
 
 }
